81a307671f080c80418fdc9a66ffdb4b04905622
sashank solasa <sashanksolasa@gmail.com>
2026-05-28 00:51:57 +0530

    after resolving all dependency related issues after configuring the service registry

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+-- Insert sample companies
+INSERT INTO "company-table" (name, description) VALUES
+('TechCorp', 'Leading software development company'),
+('InnovateTech', 'Innovation-driven technology solutions'),
+('DataDynamics', 'Big data and analytics company');
+
