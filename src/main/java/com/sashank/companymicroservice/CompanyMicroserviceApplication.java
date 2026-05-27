233c19b6ad17b6222998f8a62f4b5b57bd58e793
sashank solasa <sashanksolasa@gmail.com>
2026-05-27 16:05:47 +0530

    after resolving all dependency related issues after configuring the service registry

--- src/main/java/com/sashank/companymicroservice/CompanyMicroserviceApplication.java
+++ src/main/java/com/sashank/companymicroservice/CompanyMicroserviceApplication.java
@@ -2,7 +2,9 @@ package com.sashank.companymicroservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class CompanyMicroserviceApplication {
 
